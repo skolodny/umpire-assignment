919f196e1e7e89aa1ac638ca2a590ecd593ca023
skolodny <spencerkolodny1@gmail.com>
2026-05-01 22:00:29 -0400

    fixes

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -10,7 +10,7 @@ from app.database import get_db
 from app import models
 
 settings = get_settings()
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 
